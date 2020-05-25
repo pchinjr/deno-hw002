82c1dd55df33f3b797e9b3791c2d2206e473efd5
Paul Chin Jr <pchinjr@gmail.com>
2020-05-25 15:04:30 -0400

    bleh code

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,91 +1,30 @@
-// TODO: modify the body object!
-
-import { bar } from "./bar.ts";
-
+// src/http/get-index/index.ts
 export async function handler(req: object) {
-  const env = Deno.env.toObject();
-
+  // access environment variables
+  const env = Deno.env.toObject()
+  // Deno has browser compatible fetch() built in! 
   let data = await fetch(
-    `https://api.nasa.gov/planetary/apod?api_key=${env.NASA_KEY}`,
-  )
-    .then((response) => response.json());
-
-  console.log(env.NASA_KEY)
-
-  console.log(data.url);
-
-  console.log(bar());
-
-  let body = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
-  </head>
+  `https://api.nasa.gov/planetary/apod?api_key=${env.NASA_KEY}`
+  ).then((response) => response.json())
+  // return an HTML string
+  let body = `<!doctype html><html lang=en>
+  <head><meta charset=utf-8><title>Hi!</title></head>
   <body>
-
-    <h1 class="center-text">
-      Praise Cage, Hello NASA 
-    </h1>
-    <p><img src="${data.url}" /></p>
-
-    <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
-
+  <h1 class="center-text">Praise NASA!</h1>
+  <p><img src="${data.url}" /></p>
+  <p class="center-text">
+  Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
+  </p>
   </body>
-</html>
-`;
-
-  return {
-    statusCode: 200,
-    headers: {
-      "content-type": "text/html; charset=utf8",
-      "cache-control":
-        "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
-    },
-    body,
-  };
-}
-
-// Example responses
-
-/* Forward requester to a new path
-export async function handler (req: object) {
-  return {
-    statusCode: 302,
-    headers: {
-      'location': '/about',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+  </html>
+  `
+  // the response of our function
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "text/html; charset=utf8",
+        "cache-control": "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
+      },
+      body,
     }
   }
-}
-*/
-
-/* Respond with successful resource creation
-export async function handler (req: object) {
-  return {
-    statusCode: 201,
-    headers: {
-      'content-type': 'application/json; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
-    body: JSON.stringify({ok: true})
-  }
-}
-*/
-
-/* Deliver client-side JS
-export async function handler (req: object) {
-  return {
-    headers: {
-      'content-type': 'text/javascript; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
-    body: 'console.log("Hello world!")',
-  }
-}
-*/
